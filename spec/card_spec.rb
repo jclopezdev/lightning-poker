8ba7ae69cfe13ace9a3b3618cd84035c35e006ab
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-04-13 17:44:06 -0500

    Initial commit

---
+++ spec/card_spec.rb
@@ -0,0 +1,71 @@
+require 'set'
+require 'card'
+
+describe Card do
+  def card(params = {})
+    defaults = {
+      suit: :hearts,
+      rank: 7,
+    }
+
+    Card.build(*defaults.merge(params).values_at(:suit, :rank))
+  end
+
+  it 'has a suit' do
+    raise unless card(suit: :spades).suit == :spades
+  end
+  
+  it 'has a rank' do
+    raise unless card(rank: 4).rank == 4
+  end
+
+  it 'is hash equal to itself' do
+    subject = card(suit: :spades, rank: 4)
+    other = card(suit: :hearts, rank: 4)
+    raise unless Set.new([subject, other]).size == 1
+  end
+
+  describe 'comparing to a card of different suit' do
+    it 'is not equal' do
+      subject = card(suit: :spades, rank: 4)
+      other = card(suit: :hearts, rank: 4)
+      raise unless subject != other
+    end
+
+    it 'is not hash equal to a card of differing suit' do
+      subject = card(suit: :spades, rank: 4)
+      other = card(suit: :hearts, rank: 4)
+      raise unless Set.new([subject, other]).size == 2
+    end
+  end
+
+  it 'is not equal to a card of differing rank' do
+    subject = card(suit: :spades, rank: 4)
+    other = card(suit: :spades, rank: 5)
+    raise unless subject != other
+  end
+
+  describe 'a jack' do
+    it 'ranks higher than a 10' do
+      lower = card(rank: 10)
+      higher = card(rank: :jack)
+      raise unless higher.rank > lower.rank
+    end
+  end
+
+  describe 'a queen' do
+    it 'ranks higher than a jack' do
+      lower = card(rank: :jack)
+      higher = card(rank: :queen)
+      raise unless higher.rank > lower.rank
+    end
+  end
+
+  describe 'a king' do
+    it 'ranks higher than a queen' do
+      lower = card(rank: :queen)
+      higher = card(rank: :king)
+      raise unless higher.rank > lower.rank
+    end
+  end
+end
